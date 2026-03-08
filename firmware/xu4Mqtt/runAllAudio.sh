f46d82832a01693825b3018ad3436a6d2a946d32
jlp004 <johnlphelan2004@gmail.com>
2026-03-08 12:59:48 -0500

    runALlAudio script

---
+++ firmware/xu4Mqtt/runAllAudio.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sleep 55
+export LD_PRELOAD=/home/teamlary/.local/lib/python3.8/site-packages/scikit_learn.libs/libgomp-d22c30c5.so.1.0.0
+sleep 5
+
+kill $(pgrep -f 'python3 audioDeleter.py')
+sleep 5
+python3 audioDeleter.py &
+sleep 10
+
+kill $(pgrep -f 'python3 audioRecorder.py')
+sleep 5
+python3 audioRecorder.py &
+sleep 120
+
+kill $(pgrep -f 'audioAnalyzer.py') 
+sleep 5
+python3 audioAnalyzer.py &
+sleep 5
+
+python3 ipReader.py
+sleep 5
