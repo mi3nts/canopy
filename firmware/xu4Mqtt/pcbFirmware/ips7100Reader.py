676ffcb223777df099145e5fddfef4566ec3ef94
lakithaomal <lhw150030@utdallas.edu>
2026-06-09 17:43:08 +0000

    almost working ips7100mc topic

--- firmware/xu4Mqtt/pcbFirmware/ips7100Reader.py
+++ firmware/xu4Mqtt/pcbFirmware/ips7100Reader.py
@@ -29,6 +29,7 @@ import datetime
 from i2cMints.i2c_ips7100 import IPS7100
 from mintsXU4 import mintsSensorReader as mSR
 from mintsPMCorrections import corrections as corr
+from collections import OrderedDict
 
 debug        = False 
 bus          = smbus2.SMBus(3)
@@ -44,18 +45,36 @@ def main(loopInterval):
     ips7100_valid   = ips7100.initiate(30)
     
     startTime    = time.time()
+
     while True:
-        try:
+       try:
             print("======= IPS7100 ========")
             if ips7100_valid:
-                data = ips7100.read()
-                dateTime = datetime.datetime.now()
-                mSR.IPS7100WriteI2c(data)
-                corr.doPrediction(sensor, data, dateTime)
+                raw_data = ips7100.read()
+                dateTime = str(datetime.datetime.now())
+                mSR.IPS7100WriteI2c(raw_data)
+                pm_dict = OrderedDict([
+                    ("dateTime" , str(dateTime)),
+                    ("pc0_1"    , raw_data[0]), 
+                    ("pc0_3"    , raw_data[1]),
+                    ("pc0_5"    , raw_data[2]),
+                    ("pc1_0"    , raw_data[3]),
+                    ("pc2_5"    , raw_data[4]),
+                    ("pc5_0"    , raw_data[5]), 
+                    ("pc10_0"   , raw_data[6]),
+                    ("pm0_1"    , raw_data[7]),
+                    ("pm0_3"    , raw_data[8]),
+                    ("pm0_5"    , raw_data[9]), 
+                    ("pm1_0"    , raw_data[10]),
+                    ("pm2_5"    , raw_data[11]),
+                    ("pm5_0"    , raw_data[12]),         
+                    ("pm10_0"   , raw_data[13])
+                ])
+                corr.doPrediction(sensor, pm_dict, dateTime)
             time.sleep(.5)    
             startTime = mSR.delayMints(time.time() - startTime,loopInterval)
             
-        except Exception as e:
+       except Exception as e:
             print(e)
             time.sleep(10)
 
