d0b4051be83027c28b99f88e7e1ef341b20399fc
jlp004 <johnlphelan2004@gmail.com>
2026-09-01 14:58:10 -0500

    legacy safe_load added for compatibility

--- firmware/xu4Mqtt/pcbFirmware/orangepi/mintsXU4/mintsLatest.py
+++ firmware/xu4Mqtt/pcbFirmware/orangepi/mintsXU4/mintsLatest.py
@@ -19,7 +19,7 @@ mqttBroker          = mD.mqttBroker
 mqttCredentialsFile = mD.mqttCredentialsFile
 
 # FOR MQTT 
-credentials = yaml.load(open(mqttCredentialsFile))
+credentials = yaml.safe_load(open(mqttCredentialsFile))
 connected   = False  # Stores the connection status
 broker      = mqttBroker
 port        = mqttPort # Secure port
